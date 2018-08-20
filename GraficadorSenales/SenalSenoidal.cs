8e83938cc3141e029308b4c72c84a9787343e5ce
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2018-08-20 09:40:52 -0700

    Agregar archivos de proyecto.

---
+++ GraficadorSenales/SenalSenoidal.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FuncionSenoidal
+{
+    class SenalSenoidal
+    {
+        public double amplitud { get; set; }
+        public double fase { get; set; }
+        public double frecuencia { get; set; }
+
+        public SenalSenoidal()
+        {
+            amplitud = 1.0;
+            fase = 0.0;
+            frecuencia = 1.0;
+        }
+
+        public SenalSenoidal(double Amplitud, double Fase, double Frecuencia)
+        {
+            amplitud = Amplitud;
+            fase = Fase;
+            frecuencia = Frecuencia;
+        }
+
+        public double evaluar(double tiempo)
+        {
+            double resultado;
+            resultado = amplitud * Math.Sin((2 * Math.PI * tiempo * frecuencia) + fase);
+
+            return resultado;
+        }
+    }
+}
