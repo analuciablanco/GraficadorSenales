b67e3dbd0fd80697d84656aaf38dffd37a207248
Ana Lucia Blanco <analuciablanco@outlook.com>
2018-08-23 10:43:37 -0700

    Graficador señales funcional.

--- GraficadorSenales/MainWindow.xaml.cs
+++ GraficadorSenales/MainWindow.xaml.cs
@@ -18,23 +18,47 @@ namespace GraficadorSenales
     /// <summary>
     /// Lógica de interacción para MainWindow.xaml
     /// </summary>
+
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
 
-            plnGrafica.Points.Add(new Point(50, 20));
-            plnGrafica.Points.Add(new Point(200, 25));
-            plnGrafica.Points.Add(new Point(250, 40));
-            plnGrafica.Points.Add(new Point(400, 45));
-            plnGrafica.Points.Add(new Point(450, 30));
-            plnGrafica.Points.Add(new Point(600, 35));
-            plnGrafica.Points.Add(new Point(650, 60));
-            plnGrafica.Points.Add(new Point(800, 65));
-            plnGrafica.Points.Add(new Point(850, 40));
-            plnGrafica.Points.Add(new Point(1000, 45));
-            plnGrafica.Points.Add(new Point(1050, 70));
+
+
+        }
+
+        private void btnGraficar_Click(object sender, RoutedEventArgs e)
+        {
+            double amplitud = double.Parse(txtAmplitud.Text);
+            double fase = double.Parse(txtFase.Text);
+            double frecuencia = double.Parse(txtFrecuencia.Text);
+            double tiempoInicial = double.Parse(txtTiempoInicial.Text);
+            double tiempoFinal = double.Parse(txtTiempoFinal.Text);
+            double frecMuestreo = double.Parse(txtFrecMuestreo.Text);
+
+            double periodoMuestreo = 1 / frecMuestreo;
+
+            SenalSenoidal senal = new SenalSenoidal(amplitud, fase, frecuencia);
+
+            plnGrafica.Points.Clear();
+
+            for(double i=tiempoInicial; i<=tiempoFinal; i+=periodoMuestreo)
+            {
+                plnGrafica.Points.Add(new Point(i * scrContenedor.Width, 
+                    senal.evaluar(i) * ((scrContenedor.Height/2.0)-30)*-1 + (scrContenedor.Height/2)));
+            }
+    }
+
+        private void btnReset_Click(object sender, RoutedEventArgs e)
+        {
+            txtAmplitud.Text = "1";
+            txtFase.Text = "0";
+            txtFrecuencia.Text = "1";
+            txtTiempoInicial.Text = "0";
+            txtTiempoFinal.Text = "1";
+            txtFrecMuestreo.Text = "1000";
         }
     }
 }
